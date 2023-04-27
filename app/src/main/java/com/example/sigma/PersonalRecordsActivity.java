fd455b5042ca997416750acd2e1cfbbf914f56dc
bendruce <118769779+bendruce@users.noreply.github.com>
2023-04-27 15:19:56 +0100

    Database: Layouts: Activities: sbd personal records done - need to do user looked up exercise

--- app/src/main/java/com/example/sigma/PersonalRecordsActivity.java
+++ app/src/main/java/com/example/sigma/PersonalRecordsActivity.java
@@ -39,6 +39,18 @@ public class PersonalRecordsActivity extends AppCompatActivity {
         workoutsRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int highestRepsSquat = 0;
+                double highestWeightSquat = 0.0;
+                int previousRepsSquat = 0;
+                double previousWeightSquat = 0.0;
+                int highestRepsBench = 0;
+                double highestWeightBench = 0.0;
+                int previousRepsBench = 0;
+                double previousWeightBench = 0.0;
+                int highestRepsDead = 0;
+                double highestWeightDead = 0.0;
+                int previousRepsDead = 0;
+                double previousWeightDead = 0.0;
                 for (DataSnapshot workoutSnapshot : snapshot.getChildren()) {
                     DataSnapshot exercisesSnapshot = workoutSnapshot.child("exercises");
 
@@ -49,10 +61,7 @@ public class PersonalRecordsActivity extends AppCompatActivity {
                         // Only consider Squat, Bench, and Deadlift exercises
                         if (exerciseName.equals("Squat") || exerciseName.equals("Bench") || exerciseName.equals("Deadlift")) {
                             DataSnapshot setsSnapshot = exerciseSnapshot.child("sets");
-                            int highestReps = 0;
-                            double highestWeight = 0.0;
-                            int previousReps = 0;
-                            double previousWeight = 0.0;
+
 
                             // Loop through all the sets for the exercise
                             for (DataSnapshot setSnapshot : setsSnapshot.getChildren()) {
@@ -66,44 +75,93 @@ public class PersonalRecordsActivity extends AppCompatActivity {
                                     // Check if this set has a higher reps and weight combo than the previous highest
                                     //(weight > highestWeight) || (weight==highestWeight && reps>highestReps)
                                     //reps * weight > highestReps * highestWeight
-                                    if ((weight > highestWeight) || (weight==highestWeight && reps>highestReps)) {
-                                        previousReps = highestReps;
-                                        previousWeight = highestWeight;
-                                        highestReps = reps;
-                                        highestWeight = weight;
-                                    } else if (reps * weight > previousReps * previousWeight) {
-                                        previousReps = reps;
-                                        previousWeight = weight;
+                                    //|| (weight==highestWeight && reps>highestReps)
+
+                                    if ((weight > highestWeightSquat|| (weight==highestWeightSquat && reps>highestRepsSquat))&&"Squat".equals(exerciseName)) {
+                                        System.out.println("----------------");
+                                        System.out.println(exerciseName);
+                                        System.out.println(weight);
+                                        System.out.println(highestWeightSquat);
+                                        System.out.println("----------------");
+                                        previousRepsSquat = highestRepsSquat;
+                                        previousWeightSquat = highestWeightSquat;
+                                        highestRepsSquat = reps;
+                                        highestWeightSquat = weight;
+                                    } else if ((weight > previousWeightSquat || (weight==previousWeightSquat && reps>previousRepsSquat)) &&"Squat".equals(exerciseName)) {
+                                        previousRepsSquat = reps;
+                                        previousWeightSquat = weight;
+                                    }
+                                    if ((weight > highestWeightBench || (weight==highestWeightBench && reps>highestRepsBench) )&&"Bench".equals(exerciseName) ) {
+                                        /*
+                                        System.out.println("----------------");
+                                        System.out.println(exerciseName);
+                                        System.out.println(weight);
+                                        System.out.println(highestWeightBench);
+                                        System.out.println("----------------");
+                                        */
+
+                                        previousRepsBench = highestRepsBench;
+                                        previousWeightBench = highestWeightBench;
+                                        highestRepsBench = reps;
+                                        highestWeightBench = weight;
+                                    } else if ((weight > previousWeightBench || (weight==previousWeightBench && reps>previousRepsBench)) &&"Bench".equals(exerciseName)) {
+                                        previousRepsBench = reps;
+                                        previousWeightBench = weight;
+                                    }
+                                    if ((weight > highestWeightDead || (weight==highestWeightDead && reps>highestRepsDead))&&"Deadlift".equals(exerciseName) ) {
+                                        /*
+                                        System.out.println("----------------");
+                                        System.out.println(exerciseName);
+                                        System.out.println(weight);
+                                        System.out.println(highestWeightDead);
+                                        System.out.println("----------------");
+
+                                         */
+                                        previousRepsDead = highestRepsDead;
+                                        previousWeightDead = highestWeightDead;
+                                        highestRepsDead = reps;
+                                        highestWeightDead = weight;
+                                    } else if ((weight > previousWeightDead || (weight==previousWeightDead && reps>previousRepsDead)) &&"Deadlift".equals(exerciseName)) {
+                                        previousRepsDead = reps;
+                                        previousWeightDead = weight;
                                     }
                                 } else {
                                     Log.d("TAG", "Either reps or weight is null");
                                 }
                             }
-                            System.out.println(highestReps);
-                            System.out.println(exerciseName);
-                            System.out.println(highestWeight);
+
                             // Print the highest and previous personal records for the exercise
                             if ("Squat".equals(exerciseName)){
+                                System.out.println("******************");
+                                System.out.println(exerciseName);
+                                System.out.println(highestRepsSquat);
+                                System.out.println(highestWeightSquat);
+                                System.out.println("******************");
                                 TextView squatRepBest = findViewById(R.id.squatCurrentReps);
                                 TextView squatWeightBest = findViewById(R.id.squatCurrentWeight);
                                 TextView squatRepPrevious = findViewById(R.id.squatPrevReps2);
                                 TextView squatWeightPrevious = findViewById(R.id.squatPrevWeight2);
 
-                                squatRepBest.setText(String.valueOf(highestReps));
-                                squatWeightBest.setText(String.valueOf(highestWeight));
-                                squatRepPrevious.setText(String.valueOf(previousReps));
-                                squatWeightPrevious.setText(String.valueOf(previousWeight));
+                                squatRepBest.setText(String.valueOf(highestRepsSquat));
+                                squatWeightBest.setText(String.valueOf(highestWeightSquat));
+                                squatRepPrevious.setText(String.valueOf(previousRepsSquat));
+                                squatWeightPrevious.setText(String.valueOf(previousWeightSquat));
                             }
                             if ("Bench".equals(exerciseName)){
+                                System.out.println("******************");
+                                System.out.println(exerciseName);
+                                System.out.println(highestRepsBench);
+                                System.out.println(highestWeightBench);
+                                System.out.println("******************");
                                 TextView benchRepBest = findViewById(R.id.benchCurrentReps2);
                                 TextView benchWeightBest = findViewById(R.id.benchCurrentWeight2);
                                 TextView benchRepPrevious = findViewById(R.id.benchPrevReps2);
                                 TextView benchWeightPrevious = findViewById(R.id.benchPrevWeight2);
 
-                                benchRepBest.setText(String.valueOf(highestReps));
-                                benchWeightBest.setText(String.valueOf(highestWeight));
-                                benchRepPrevious.setText(String.valueOf(previousReps));
-                                benchWeightPrevious.setText(String.valueOf(previousWeight));
+                                benchRepBest.setText(String.valueOf(highestRepsBench));
+                                benchWeightBest.setText(String.valueOf(highestWeightBench));
+                                benchRepPrevious.setText(String.valueOf(previousRepsBench));
+                                benchWeightPrevious.setText(String.valueOf(previousWeightBench));
                             }
                             if ("Deadlift".equals(exerciseName)){
                                 TextView deadRepBest = findViewById(R.id.deadCurrentReps);
@@ -111,10 +169,10 @@ public class PersonalRecordsActivity extends AppCompatActivity {
                                 TextView deadRepPrevious = findViewById(R.id.deadPrevReps);
                                 TextView deadWeightPrevious = findViewById(R.id.deadPrevWeight2);
 
-                                deadRepBest.setText(String.valueOf(highestReps));
-                                deadWeightBest.setText(String.valueOf(highestWeight));
-                                deadRepPrevious.setText(String.valueOf(previousReps));
-                                deadWeightPrevious.setText(String.valueOf(previousWeight));
+                                deadRepBest.setText(String.valueOf(highestRepsDead));
+                                deadWeightBest.setText(String.valueOf(highestWeightDead));
+                                deadRepPrevious.setText(String.valueOf(previousRepsDead));
+                                deadWeightPrevious.setText(String.valueOf(previousWeightDead));
                             }
 
 
