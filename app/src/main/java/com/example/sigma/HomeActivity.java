8de4dec24fe8a662611f406b9054cee9d78c4c92
bendruce <118769779+bendruce@users.noreply.github.com>
2023-05-01 15:09:44 +0100

    Database: Layouts: Activities:
    creating a webview for the html guide

--- app/src/main/java/com/example/sigma/HomeActivity.java
+++ app/src/main/java/com/example/sigma/HomeActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -210,6 +212,15 @@ public class HomeActivity extends AppCompatActivity {
                 // create and show the dialog
                 AlertDialog dialog = builder.create();
                 dialog.show();
+            }
+        });
+        ImageButton guideButton = findViewById(R.id.openGuideButton);
+// set the click listener for the addWorkoutFolderBtn button
+        guideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, UserGuideActivity.class);
+                startActivity(i);
             }
         });
 
---
+++ app/src/main/java/com/example/sigma/UserGuideActivity.java
@@ -0,0 +1,24 @@
+package com.example.sigma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class UserGuideActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_guide);
+
+        webView = findViewById(R.id.guideWebView);
+        //webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_assets/guide.html");
+
+    }
+}
